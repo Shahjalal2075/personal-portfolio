ca1c95b021546daf474b1f7dd9b23764006694a9
Shahjalal2075 <shahjalal2075@gmail.com>
2023-12-11 13:51:27 +0600

    footer add

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,9 +1,29 @@
+import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <div>
-            <h2 className="text-base text-center font-semibold py-10">© 2023 Shahjalal. All right reserved.</h2>
-        </div>
+        <footer className="footer footer-center p-20">
+            <aside>
+                <Link to={"/"}>
+                    <img className="w-16" src="https://i.ibb.co/k0mFjS3/9211049.png" alt="SJ" />
+                </Link>
+                <p className="mt-6">© 2023 Shahjalal. All right reserved.</p>
+            </aside>
+            <nav>
+                <div className="flex gap-6">
+                    <a href="https://www.linkedin.com/in/shahjalal2075/" className="border rounded-full p-3 text-xl flex items-center gap-3">
+                        <FaLinkedinIn />
+                    </a>
+                    <a href="https://www.facebook.com/shahjalal2075/" className="border rounded-full p-3 text-xl flex items-center gap-3">
+                        <FaFacebookF />
+                    </a>
+                    <a href="https://www.twitter.com/shahjalal2075/" className="border rounded-full p-3 text-xl flex items-center gap-3">
+                        <FaTwitter />
+                    </a>
+                </div>
+            </nav>
+        </footer>
     );
 };
 
